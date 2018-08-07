868115434e7de83dafa849c23618068198c67c98
Maxim_F <maxim.9408@mail.ru>
2018-08-07 22:08:23 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,46 +1,74 @@
 'use strict';
-
+// Коды клавиш
 const RIGHT_ARROW = 39;
 const LEFT_ARROW = 37;
 
-const mainElement = document.querySelector(`#main`);
+const sliderContainer = document.querySelector(`#main`);
 
 const selectSlide = (element) => {
-  mainElement.innerHTML = ``;
-  mainElement.appendChild(element.cloneNode(true));
+  while (sliderContainer.firstChild) {
+    sliderContainer.removeChild(sliderContainer.firstChild);
+  }
+  sliderContainer.appendChild(element.cloneNode(true));
 };
 
 const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
 
 let current = 0;
-const select = (index) => {
+const showSlide = (index) => {
   index = index < 0 ? screens.length - 1 : index;
   index = index >= screens.length ? 0 : index;
   current = index;
   selectSlide(screens[current]);
 };
 
-document.addEventListener(`keydown`, (evt) => {
+// Функция переключения слайдов клавишами
+const keyPress = (evt) => {
   switch (evt.keyCode) {
     case RIGHT_ARROW:
-      select(current + 1);
+      showSlide(current + 1);
       break;
     case LEFT_ARROW:
-      select(current - 1);
+      showSlide(current - 1);
       break;
   }
-});
+};
 
+// Добавляем визуальные стрелки
+document.querySelector(`body`).insertAdjacentHTML(`beforeEnd`, `
+<div class="arrows__wrap">
+  <style>
+    .arrows__wrap {
+      position: absolute;
+      top: 95px;
+      left: 50%;
+      margin-left: -56px;
+      z-index: 10;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn"><-</button>
+  <button class="arrows__btn">-></button>
+</div>
+`);
+
+// элементы управления
 const arrows = document.querySelector(`.arrows__wrap`);
 const nextSlide = arrows.querySelector(`.arrows__btn:last-child`);
 const prevSlide = arrows.querySelector(`.arrows__btn`);
 
+document.addEventListener(`keydown`, keyPress);
+
 nextSlide.addEventListener(`click`, () => {
-  select(current + 1);
+  showSlide(current + 1);
 });
 
 prevSlide.addEventListener(`click`, () => {
-  select(current - 1);
+  showSlide(current - 1);
 });
 
-select(1);
+showSlide(1);
