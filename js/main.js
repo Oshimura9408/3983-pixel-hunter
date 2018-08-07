b98bf230e7e9f1c696e5068d8e0172e90dbb2796
Maxim_F <maxim.9408@mail.ru>
2018-08-07 10:47:09 +0300

    Начинаю программировать

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const RIGHT_ARROW = 37;
+const LEFT_ARROW = 39;
+
+const mainElement = document.querySelector(`#main`);
+
+const selectSlide = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
+};
+
+const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
+
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(screens[current]);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case RIGHT_ARROW:
+      select(current - 1);
+      break;
+    case LEFT_ARROW:
+      select(current + 1);
+      break;
+  }
+});
+
+const arrows = document.querySelector(`.arrows__wrap`);
+const nextSlide = arrows.querySelector(`.arrows__btn:last-child`);
+const prevSlide = arrows.querySelector(`.arrows__btn`);
+
+nextSlide.addEventListener(`click`, () => {
+  select(current + 1);
+});
+
+prevSlide.addEventListener(`click`, () => {
+  select(current - 1);
+});
+
+select(0);
