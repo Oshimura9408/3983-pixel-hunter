b4cb7a8fb59eb2186b0108105ff10be322faa628
Maxim_F <maxim.9408@mail.ru>
2018-08-12 15:38:45 +0300

    Найти решение сброса

---
+++ js/intro.js
@@ -0,0 +1,24 @@
+import {selectSlide, render} from './util.js';
+import greeting from './greeting.js';
+
+const template = `
+<section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </section>
+`;
+
+const element = render(template);
+
+const startButton = element.querySelector(`.intro__asterisk`);
+
+const changeScreen = () => {
+  selectSlide(greeting);
+};
+
+// startButton.addEventListener(`click`, () => {
+//   changeScreen(greeting);
+// });
+startButton.addEventListener(`click`, changeScreen);
+
+export default element;
--- js/main.js
+++ js/main.js
@@ -1,76 +1,79 @@
-'use strict';
-// Коды клавиш
-const RIGHT_ARROW = 39;
-const LEFT_ARROW = 37;
+// // Коды клавиш
+// const RIGHT_ARROW = 39;
+// const LEFT_ARROW = 37;
+//
+// const sliderContainer = document.querySelector(`#main`);
+//
+// const selectSlide = (element) => {
+//   sliderContainer.innerHTML = ``;
+//   sliderContainer.appendChild(element.cloneNode(true));
+// };
+//
+// const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
+//
+// let current = 0;
+// const showSlide = (index) => {
+//   let sliderNumber;
+//   const len = screens.length - 1;
+//   if (index > 0 && index <= len) {
+//     sliderNumber = index;
+//     current = sliderNumber;
+//   }
+//   selectSlide(screens[current]);
+// };
+//
+// const onKeyDown = (evt) => {
+//   switch (evt.keyCode) {
+//     case RIGHT_ARROW:
+//       showSlide(current + 1);
+//       break;
+//     case LEFT_ARROW:
+//       showSlide(current - 1);
+//       break;
+//   }
+// };
+//
+// // Добавляем визуальные стрелки
+// document.querySelector(`body`).insertAdjacentHTML(`beforeEnd`, `
+// <div class="arrows__wrap">
+//   <style>
+//     .arrows__wrap {
+//       position: absolute;
+//       top: 95px;
+//       left: 50%;
+//       margin-left: -56px;
+//       z-index: 10;
+//     }
+//     .arrows__btn {
+//       background: none;
+//       border: 2px solid black;
+//       padding: 5px 20px;
+//     }
+//   </style>
+//   <button class="arrows__btn"><-</button>
+//   <button class="arrows__btn">-></button>
+// </div>
+// `);
+//
+// const arrows = document.querySelector(`.arrows__wrap`);
+// const nextSlideButton = arrows.querySelector(`.arrows__btn:last-child`);
+// const prevSlideButton = arrows.querySelector(`.arrows__btn`);
+//
+// const onArrowLeftPress = () => {
+//   showSlide(current - 1);
+// };
+//
+// const onArrowRightPress = () => {
+//   showSlide(current + 1);
+// };
+//
+// nextSlideButton.addEventListener(`click`, onArrowRightPress);
+// prevSlideButton.addEventListener(`click`, onArrowLeftPress);
+//
+// document.addEventListener(`keydown`, onKeyDown);
+//
+// showSlide(1);
+import {selectSlide} from "./util.js";
+import intro from './intro.js';
 
-const sliderContainer = document.querySelector(`#main`);
-
-const selectSlide = (element) => {
-  sliderContainer.innerHTML = ``;
-  sliderContainer.appendChild(element.cloneNode(true));
-};
-
-const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
-
-let current = 0;
-const showSlide = (index) => {
-  let sliderNumber;
-  const len = screens.length - 1;
-  if (index > 0 && index <= len) {
-    sliderNumber = index;
-    current = sliderNumber;
-  }
-  selectSlide(screens[current]);
-};
-
-const onKeyDown = (evt) => {
-  switch (evt.keyCode) {
-    case RIGHT_ARROW:
-      showSlide(current + 1);
-      break;
-    case LEFT_ARROW:
-      showSlide(current - 1);
-      break;
-  }
-};
-
-// Добавляем визуальные стрелки
-document.querySelector(`body`).insertAdjacentHTML(`beforeEnd`, `
-<div class="arrows__wrap">
-  <style>
-    .arrows__wrap {
-      position: absolute;
-      top: 95px;
-      left: 50%;
-      margin-left: -56px;
-      z-index: 10;
-    }
-    .arrows__btn {
-      background: none;
-      border: 2px solid black;
-      padding: 5px 20px;
-    }
-  </style>
-  <button class="arrows__btn"><-</button>
-  <button class="arrows__btn">-></button>
-</div>
-`);
-
-const arrows = document.querySelector(`.arrows__wrap`);
-const nextSlideButton = arrows.querySelector(`.arrows__btn:last-child`);
-const prevSlideButton = arrows.querySelector(`.arrows__btn`);
-
-const onArrowLeftPress = () => {
-  showSlide(current - 1);
-};
-
-const onArrowRightPress = () => {
-  showSlide(current + 1);
-};
-
-nextSlideButton.addEventListener(`click`, onArrowRightPress);
-prevSlideButton.addEventListener(`click`, onArrowLeftPress);
-
-document.addEventListener(`keydown`, onKeyDown);
-
-showSlide(1);
+selectSlide(intro);
